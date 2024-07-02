52c605e694e284702773246157944ed3b465f006
Diomede T <diomede.tripicchio@wfp.org>
2024-07-02 13:57:40 +0200

    add keymaps for cmp

--- config/cmp.nix
+++ config/cmp.nix
@@ -11,6 +11,22 @@
 	{ name = "path"; }
 	{ name = "buffer"; }
       ];
+      
+      mappings = {
+	"<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+	"<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+
+	"<C-p>" = "cmp.mapping.select_prev_item()";
+        "<C-n>" = "cmp.mapping.select_next_item()";
+	"<Up>" = "cmp.mapping.select_prev_item()";
+        "<Down>" = "cmp.mapping.select_next_item()";
+
+        "<C-Space>" = "cmp.mapping.complete()";
+	"<C-d>" = "cmp.mapping.scroll_docs(-4)";
+	"<C-e>" = "cmp.mapping.close()";
+	"<C-f>" = "cmp.mapping.scroll_docs(4)";
+	"<CR>" = "cmp.mapping.confirm({ select = true })";
+      };
     };
   };
 }
